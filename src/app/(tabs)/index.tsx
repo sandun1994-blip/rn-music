a0a7d07be029af2c87048a0ed232cc43f47f1292
Sandun Tharuka <sandun.suntws@gmail.com>
2023-10-11 13:00:35 +0530

    setup home screen

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet,FlatList } from 'react-native';
+import { View,Text } from '../../components/Themed';
+import { tracks } from '../../../assets/data/tracks';
+import TrackListItem from '../../components/TrackListItem';
+
+
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+     
+     <FlatList data={tracks} renderItem={({item})=> <TrackListItem key={item.id} track={item}/>} showsVerticalScrollIndicator={false}/>
+    </View>
+  );
+}
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ 
+});
---
+++ src/components/TrackListItem.tsx
@@ -0,0 +1,48 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Track } from "../../assets/data/types";
+
+const TrackListItem = ({ track }: { track: Track }) => {
+  console.log(track);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: track.album.images[0]?.url }}
+        style={styles.image}
+      />
+      <View style={{}}>
+      <Text style={styles.title}>{track.name}</Text>
+      <Text style={styles.subtitle}>{track.artists[0].name}</Text>
+      </View>
+      
+    </View>
+  );
+};
+
+export default TrackListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 5,
+    padding: 5,
+    flexDirection:'row',
+    alignItems:'center',
+    width:'100%'
+    
+  },
+  title: {
+    color: "gray",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+  subtitle: {
+    color: "gray",
+  },
+  image: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 5,
+    marginRight:10
+  },
+});
